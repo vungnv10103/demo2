431d47cf1a281a76244668249576d6b1744939c8
VungNguyen <vungvung1001@gmail.com>
2022-11-16 15:10:48 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.*;;
+public class main {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+}
